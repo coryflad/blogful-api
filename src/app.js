35880d7ff0689b2d64b13459c5b0226e8b0a4a83
Cory Fladhammer <cjfladham@hotmail.com>
2020-07-09 18:59:04 -0500

    add GET /articles

--- src/app.js
+++ src/app.js
@@ -3,29 +3,39 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
-const { NODE_ENV }  = require('./config')
+const { NODE_ENV } = require('./config')
+const ArticlesService = require('./articles-service')
 
 const app = express()
 
 const morganOption = (NODE_ENV === 'production')
-? 'tiny'
-: 'common';
+    ? 'tiny'
+    : 'common';
 
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
+app.get('/articles', (req, res, next) => {
+    const knexInstance = req.app.get('db')
+    ArticlesService.getAllArticles(knexInstance)
+        .then(articles => {
+            res.json(articles)
+        })
+        .catch(next)
+})
+
 app.get('/', (req, res) => {
-    res.send('Hello, world!')
+    res.send('Fuck you world!')
 })
 
 app.use(function errorHandler(error, req, res, next) {
     let response
     if (NODE_ENV === 'production') {
-        response = { error: { message: 'server error'} }
+        response = { error: { message: 'server error' } }
     } else {
         console.error(error)
-        response = { message: errror.message, error}
+        response = { message: errror.message, error }
     }
     res.status(500).json(response)
 })
